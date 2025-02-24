056c61c3b3bf8b7555df1c51afd2135b3b2fcac4
Priyanshu Katiyar <priyanshu.katiyar@easyhfc.com>
2025-02-25 01:31:15 +0530

    Port Assigned

--- main.py
+++ main.py
@@ -323,4 +323,5 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # Run the app
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
